82fbc14924974835145c4dd0202f3e7d438c7797
lihuiruo101 <yjy140206@gmail.com>
2025-05-02 13:20:54 +0900

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,23 @@
+from __init__
+import __init__
+
+Btf=true
+if korea = server_took (30)
+delete () server_front
+elif editor = save_took (40)
+prompt () save_desks
+else =
+      exit() whale_()
+hack_email = ("please enter your email : ")
+save_stats {hack_email}
+save familylink_tesks
+print ("check save data..")
+Btf save data
+print ("check save key..")
+Btf save key
+print ("checking save server..")
+Btf save server
+if True all = edit.py
+if False =
+          print ("It is not code.")
+whale ()
