6d3ad3e3f6d177d31d1f3484a33ed7c6952b38c7
lihuiruo101 <yjy140206@gmail.com>
2025-05-02 13:14:09 +0900

    Create __init__.py
    main_menu

---
+++ __init__.py
@@ -0,0 +1,33 @@
+"""unit_play"""
+save data () logic_(UTF-8)
+True Korea_false desks
+import os
+from edit.play_time
+inp=input
+
+"""setting desks"""
+save data () logic_(UTF-8)
+inp ("""logic""")
+
+server = ("https://families.google.com/familylink")
+front = ("https://editor.chrome.com")
+
+dev_tools True () {server}
+/, *, init 30 > {front}
+if front < 30 =
+               edit_desks to_(front)
+front_server= * 30 * 50
+
+frost_input () 
+import familylink
+import requests
+from familylink()
+
+from __main__ __rook__
+print (/"delete = https://familylink.com"/)
+print (/front = {front}_30}
+
+server = IP_verver
+IP_verver = familylink_verver
+save_data()
+main_menu__init
